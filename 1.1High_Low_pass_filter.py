03a3abe03a607cad358b2d26ce2fc6cd0f87758e
Ildar <de.ikar@yandex.ru>
2020-10-11 23:17:51 +0900

    Update 1.1High_Low_pass_filter.py

--- 1.1High_Low_pass_filter.py
+++ 1.1High_Low_pass_filter.py
@@ -1,57 +1,69 @@
 import numpy as np
-from scipy.signal import butter, lfilter, freqz
+import pandas as pd
+from scipy import signal
 import matplotlib.pyplot as plt
+import serial
+import time
 
+ComPort = serial.Serial('COM13') 
+ComPort.baudrate = 115200          
+ComPort.bytesize = 8            
+ComPort.parity   = 'N'           
+ComPort.stopbits = 1
+random_data = np.arange(50)
 
-def butter_lowpass(cutoff, fs, order=5):
+def receive_data():
+ for i in range(0,50,1):
+  random_data[i] = int(ComPort.readline())  
+ result = pd.DataFrame({'data': random_data} )
+ return result
+
+def butter_highpass(cutoff, fs, order=3):
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
     return b, a
 
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    y = lfilter(b, a, data)
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+def butter_lowpass(cutoffs, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoffs, fs, order=5):
+    b, a = butter_lowpass(cutoffs, fs, order=order)
+    y = signal.lfilter(b, a, data)
     return y
 
 
-# Filter requirements.
-order = 6
-fs = 30.0       # sample rate, Hz
-cutoff = 3.667  # desired cutoff frequency of the filter, Hz
+#order = 6
+fps=fs = 10     # sample rate, Hz
+cutoffs = 30
+cutoff=2
 
-# Get the filter coefficients so we can check its frequency response.
-b, a = butter_lowpass(cutoff, fs, order)
+figure, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
-# Plot the frequency response.
-w, h = freqz(b, a, worN=8000)
-plt.subplot(2, 1, 1)
-plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
-plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
-plt.axvline(cutoff, color='k')
-plt.xlim(0, 0.5*fs)
-plt.title("Lowpass Filter Frequency Response")
-plt.xlabel('Frequency [Hz]')
-plt.grid()
+axis_x=0
+while 1:
+ sine = receive_data()
+ filtered_sine = butter_highpass_filter(sine.data, cutoff, fps)
+ filtered_sine  =  butter_lowpass_filter(filtered_sine.data, cutoffs, fps)
+ 
+ 
+ ax1.plot(range(axis_x, axis_x+50,1),sine,color = '#0a0b0c') 
+ ax2.plot(range(axis_x, axis_x+50,1),filtered_sine, color = '#0a0b0c')
+ axis_x1_move=sine["data"]
+ ax1.axis([axis_x-4000, axis_x+2000, axis_x1_move[48]-200000, axis_x1_move[48]+200000])
+ ax2.axis([axis_x-4000, axis_x+2000, filtered_sine[48]-50, filtered_sine[48]+50]) 
+ axis_x=axis_x+50
 
+ 
+ plt.pause(0.001)
+ plt.draw()
 
-# Demonstrate the use of the filter.
-# First make some data to be filtered.
-T = 5.0         # seconds
-n = int(T * fs) # total number of samples
-t = np.linspace(0, T, n, endpoint=False)
-# "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
-
-# Filter the data, and plot both the original and filtered signals.
-y = butter_lowpass_filter(data, cutoff, fs, order)
-
-plt.subplot(2, 1, 2)
-plt.plot(t, data, 'b-', label='data')
-plt.plot(t, y, 'g-', linewidth=2, label='filtered data')
-plt.xlabel('Time [sec]')
-plt.grid()
-plt.legend()
-
-plt.subplots_adjust(hspace=0.35)
-plt.show()
+#plt.show()
