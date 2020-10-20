acc84f73ea09299d1cd9fc580c9c4f234933606e
Ildar <de.ikar@yandex.ru>
2020-10-20 15:45:32 +0900

    Update 1.GUI.py

--- GUI/1.GUI.py
+++ GUI/1.GUI.py
@@ -9,9 +9,9 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 from PyQt5.Qt import *
-
 import random
 import threading 
+import time
 
 
 #global value
@@ -21,7 +21,7 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
                
         self.setMinimumSize(QSize(300, 200))    
-        self.setWindowTitle("PyQt button example - pythonprogramminglanguage.com") 
+        self.setWindowTitle("BCI") 
         pybutton = QPushButton('Click me', self)        
         pybutton.clicked.connect(self.clickMethod)
         pybutton.resize(100,32)
@@ -60,54 +60,48 @@ class second_window(QWidget):
         layout = QVBoxLayout()
         layout.setContentsMargins(50,100,0,11) # move background
         layout.setGeometry(QRect(0, 0, 80, 68))# nothing  
-      #  layout.addWidget(self.toolbar)
+      # layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)    
-      #  layout.addWidget(self.toolbar1)
+      # layout.addWidget(self.toolbar1)
         layout.addWidget(self.canvas1)        
-       # 
+       
         self.le_num1 = QLineEdit()
         self.le_num1.setFixedSize(50, 20) # size
-        
-                
+                        
         self.pb_num1 = QPushButton('...')
         self.pb_num1.setFixedSize(50, 60) # size
         self.pb_num1.clicked.connect(self.show_dialog_num1)       
         
         #layout1 = QGridLayout()        
         layout.addWidget(QLabel('Scale'))
-
-                
+               
         layout.addWidget(self.le_num1)       
         self.pb_num1.move(10, 0)
         
         layout.addWidget(self.pb_num1)
         self.setLayout(layout)
-
- 
-
-    
-        
+   
     def clickMethod(self):
+      #  while 1:
+         print("type")
+         time.sleep(0.5)
+         data = [random.random() for i in range(axis)]
+         data1 = [random.random() for i in range(axis)]
+         self.figure.clear()
+         self.figure1.clear()
         
-        print('parampam')
+         ax = self.figure.add_subplot(111)
+         ax1 = self.figure1.add_subplot(111)
         
-        data = [random.random() for i in range(axis)]
-        data1 = [random.random() for i in range(axis)]
-        self.figure.clear()
-        self.figure1.clear()
-        
-        ax = self.figure.add_subplot(111)
-        ax1 = self.figure1.add_subplot(111)
-        
-        ax.plot(data, '*-')
-        ax1.plot(data1, '*-')
-        self.canvas.draw()
-        self.canvas1.draw()
-        
+         ax.plot(data, '*-')
+         ax1.plot(data1, '*-')
+         self.canvas.draw()
+         self.canvas1.draw()
 
-
-        
-        
+    #     z=self.clickMethod 
+    #     thread = threading.Thread(target=clickMethod(self))
+         thread=threading.Thread(target=self.clickMethod, args=())
+         thread.start()                
 # input data
     def show_dialog_num1(self):
         value, r = QInputDialog.getInt(self, 'Input dialog', 'Enter your num1:')
