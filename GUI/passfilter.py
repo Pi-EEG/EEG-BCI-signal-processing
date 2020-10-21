de7e2fe2cd5878aba3a2b381e32e40fcba9eb8ff
Ildar <de.ikar@yandex.ru>
2020-10-22 00:12:04 +0900

    Update passfilter.py

--- GUI/passfilter.py
+++ GUI/passfilter.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import serial
 import time
 
-fps=fs = 10     
-cutoffs = 2
-cutoff=2
+#fs = 10     
+#cutoffs = 2
+#cutoff=2
 
 def butter_highpass(cutoff, fs, order=3):
     nyq = 0.5 * fs
@@ -15,7 +15,7 @@ def butter_highpass(cutoff, fs, order=3):
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
     return b, a
 
-def butter_highpass_filter(data,order=5):
+def butter_highpass_filter(data, cutoff, fs, order=5):
     b, a = butter_highpass(cutoff, fs, order=order)
     y = signal.filtfilt(b, a, data)
     return y
@@ -26,10 +26,8 @@ def butter_lowpass(cutoff, fs, order=5):
     b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
     return b, a
 
-def butter_lowpass_filter(data, order=5):
+def butter_lowpass_filter(data, cutoff, fs, order=5):
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = signal.lfilter(b, a, data)
     
     return y
-
-
