e7312d0280c1dac77e6fb7a43a4cf77b73ed901f
Ildar <de.ikar@yandex.ru>
2020-10-22 00:11:38 +0900

    Update 1.GUI.py

--- GUI/1.GUI.py
+++ GUI/1.GUI.py
@@ -18,14 +18,17 @@ import serial
 import passfilter 
 
 
-ComPort = serial.Serial('COM5') 
+ComPort = serial.Serial('COM8') 
 ComPort.baudrate = 115200          
 ComPort.bytesize = 8            
 ComPort.parity   = 'N'           
 ComPort.stopbits = 1
 random_data = np.arange(50)
 
-
+global fs
+fs = 10     
+global cutoff
+cutoff = 2
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -44,6 +47,7 @@ class MainWindow(QMainWindow):
         mainWin.close()
 
 class second_window(QWidget):
+
     def __init__(self):
         print ("start")       
         QWidget.__init__(self)
@@ -81,25 +85,42 @@ class second_window(QWidget):
         layout.addWidget(self.canvas)    
       # layout.addWidget(self.toolbar1)
         layout.addWidget(self.canvas1)        
-       
+
+       # start for add cufoff value
         self.le_num1 = QLineEdit()
-        self.le_num1.setFixedSize(50, 20) # size
-                        
-        self.pb_num1 = QPushButton('...')
+        self.le_num1.setFixedSize(50, 20) # size                        
+        self.pb_num1 = QPushButton('cutoff')
         self.pb_num1.setFixedSize(50, 60) # size
-        self.pb_num1.clicked.connect(self.show_dialog_num1)       
-        
+        self.pb_num1.clicked.connect(self.show_dialog_num1)               
         #layout1 = QGridLayout()        
-        layout.addWidget(QLabel('Scale'))
+       # layout.addWidget(QLabel('cutoff'))
+        # stop for add cufoff value
+
+        # start for add fps
+        self.le_num2 = QLineEdit()
+        self.le_num2.setFixedSize(50, 20) # size                        
+        self.pb_num2 = QPushButton('fs')
+        self.pb_num2.setFixedSize(50, 60) # size
+        self.pb_num2.clicked.connect(self.show_dialog_num2)               
+        #layout1 = QGridLayout()        
+       # layout.addWidget(QLabel('fs'))
+        # start for add fps
+
+
+
                
         layout.addWidget(self.le_num1)       
-        self.pb_num1.move(10, 0)
-        
+        self.pb_num1.move(10, 0)        
         layout.addWidget(self.pb_num1)
         self.setLayout(layout)
+
+        layout.addWidget(self.le_num2)       
+        self.pb_num1.move(90, 100)        
+        layout.addWidget(self.pb_num2)
+        self.setLayout(layout)
     
- 
     def clickMethod(self):
+
       #  while 1:
          try:
           for i in range(0,50,1):
@@ -107,11 +128,11 @@ class second_window(QWidget):
            random_data[i] = int(ComPort.readline())
            
           result = pd.DataFrame({'data': random_data} )          
-          result = passfilter.butter_highpass_filter(result.data)                  
+          result = passfilter.butter_highpass_filter(result.data,cutoff, fs)                  
           result = pd.DataFrame({'data': result} )          
           result['data']=result['data'].astype('int')        
          #print (result)
-          result  =  passfilter.butter_lowpass_filter(result.data)  
+          result  =  passfilter.butter_lowpass_filter(result.data,cutoff, fs)  
          #print (result)
            
          except ValueError:
@@ -136,7 +157,7 @@ class second_window(QWidget):
          global axis_x
 
          ax.plot(range(axis_x, axis_x+50,1),data,color = '#0a0b0c') 
-         ax.axis([axis_x-500, axis_x+500, bias-1500, bias+1500])  #
+         ax.axis([axis_x-50, axis_x+50, bias-500, bias+500])  #
          
          ax1.plot(range(axis_x, axis_x+50,1),data) 
          ax1.axis([axis_x-500, axis_x+500, bias-5000, bias+5000])  #
@@ -150,10 +171,15 @@ class second_window(QWidget):
          thread.start()                
 # input data
     def show_dialog_num1(self):
-        value, r = QInputDialog.getInt(self, 'Input dialog', 'Enter your num1:')
-        global axis
-        axis = value
-        print (axis)
+        value, r = QInputDialog.getInt(self, 'Input dialog', 'cut frecuency:')
+        global cutoff
+        cutoff = value
+        print (cutoff)
+    def show_dialog_num2(self):
+        value, r = QInputDialog.getInt(self, 'Input dialog', 'fs:')
+        global fs
+        fs = value
+        print (fs)
         
        # thread.start()
        # mainWin.show()
